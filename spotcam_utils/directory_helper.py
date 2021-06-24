95c0a41d7c83ddd32001de102eea2867152f0dd7
Andrew-mie7 <63439849+Andrew-mie7@users.noreply.github.com>
2021-06-25 02:13:57 +0800

    Minor bugs

--- spotcam_utils/directory_helper.py
+++ spotcam_utils/directory_helper.py
@@ -49,7 +49,8 @@ class DirectoryHelper(BaseSettings):
             os.mkdir(self.RENAMED)
             logging.info(f'mkdir: {self.RENAMED}')
 
+        self.config = ConfigHelper()
         self.FTP = {
-            'BIME-YunLin': os.path.join(ConfigHelper.get_FTPDIR(), '雲林斗六'),
-            'BIME-ChiaYi': os.path.join(ConfigHelper.get_FTPDIR(), '嘉義太保')
+            'BIME-YunLin': os.path.join(self.config.get_FTPDIR(), '雲林斗六'),
+            'BIME-ChiaYi': os.path.join(self.config.get_FTPDIR(), '嘉義太保')
         }
